7131d8e3410bed8030950fb3516e18674d37dc01
SheikhMaheer <sheikh.maheer@outlook.com>
2025-12-22 04:24:12 +0600

    add selected curreccy on select into dropdown

--- script.js
+++ script.js
@@ -21,20 +21,19 @@ Mode.addEventListener("click", () => {
 
 
 
-
+console.log("The Supported Countries Are:");
 let dropDown = document.querySelectorAll(".currency select");
 for (const select of dropDown) {
-    console.log("The Supported Countries Are:");
     for (let country in countries) {
         console.log(`${Number(country)+1}) ${countries[country].name}`);
         let option = document.createElement("option");
         option.value = country;
         option.innerText = countries[country].currency.code;
         select.appendChild(option);
-        // if () {
-            
-        // } else {
-            
-        // }
+        if (select.name == "selectFrom" && option.innerText == "USD") {
+            option.selected = "selected";
+        } else if (select.name == "selectTo" && option.innerText == "BDT") {
+            option.selected = "selected";
+        }
     }
 }
