341aba1e33588c3dbf2351486803f026ffda2d19
SheikhMaheer <sheikh.maheer@outlook.com>
2025-12-22 03:52:24 +0600

    add select on dropdown

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//theme changer
+
 let Mode = document.getElementsByClassName("theme-toggle")[0];
 let body = document.body;
 Mode.addEventListener("click", () => {
@@ -7,3 +9,32 @@ Mode.addEventListener("click", () => {
         body.classList.add("dark");
     }
 });
+
+
+
+
+
+
+
+
+// let URL = "https://api.github.com/users/";
+
+
+
+
+let dropDown = document.querySelectorAll(".currency select");
+for (const select of dropDown) {
+    console.log("The Supported Countries Are:");
+    for (let country in countries) {
+        console.log(`${Number(country)+1}) ${countries[country].name}`);
+        let option = document.createElement("option");
+        option.value = country;
+        option.innerText = countries[country].currency.code;
+        select.appendChild(option);
+        // if () {
+            
+        // } else {
+            
+        // }
+    }
+}
